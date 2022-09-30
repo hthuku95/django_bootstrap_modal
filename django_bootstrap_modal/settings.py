2ef0e0684b57eb248b8feafbda7b5525b853e3f0
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-09-30 10:52:09 +0300

    removed django_crontab

--- django_bootstrap_modal/settings.py
+++ django_bootstrap_modal/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
